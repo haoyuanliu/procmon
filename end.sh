cb34906f1886fd8a46a2eca62667223bb3fd1ed3
zuilehongdou <15120101@bjtu.edu.cn>
2017-07-26 22:11:23 +0800

    UPDATE: update start and end file

---
+++ end.sh
@@ -0,0 +1,3 @@
+#/bin/zsh
+kill `cat pid.txt`
+killall procmon
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #/bin/zsh
-killall procmon
 killall ./dummyload
+killall procmon
 ./dummyload z 60 1 > pid.txt &
 spawn-fcgi -a 127.0.0.1 -p 8082 -f ./procmon
